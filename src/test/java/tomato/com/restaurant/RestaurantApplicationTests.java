866e654331a9df764727872a6bfb87c2484620ef
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-10 02:38:28 +0530

    Add project structure

---
+++ src/test/java/tomato/com/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package tomato.com.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
