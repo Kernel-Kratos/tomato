652a3cd9762bf8bf70ad0a7c64f2103c2ac88a14
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-21 23:47:18 +0530

    Add Role Constants

---
+++ src/main/java/tomato/com/restaurant/data/RoleConstants.java
@@ -0,0 +1,7 @@
+package tomato.com.restaurant.data;
+
+public final class RoleConstants {
+    public final static String customer = "Role_Customer";
+    public final static String owner ="Role_Restaurant_Owner";
+
+}
