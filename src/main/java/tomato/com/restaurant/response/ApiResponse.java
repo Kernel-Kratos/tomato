aa01b1519add404493185a5336b95d2678ab3163
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-21 22:33:20 +0530

    Add ApiResponse template

---
+++ src/main/java/tomato/com/restaurant/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package tomato.com.restaurant.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+    private String message;
+    private Object data;
+
+    public ApiResponse(Object data){
+        this.data = data;
+    }
+}
