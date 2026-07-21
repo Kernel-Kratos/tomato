f2b0aa1e9f0bb0718e773875011278466b280be7
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-21 22:27:47 +0530

    Add entities

---
+++ src/main/java/tomato/com/restaurant/model/Permission.java
@@ -0,0 +1,30 @@
+package tomato.com.restaurant.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String permissionName;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+   
+}
