e6f67cea7caef73d7cd5fc8c9e87d46b8b03f2aa
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-29 00:06:44 +0530

    Add refresh token entity

---
+++ src/main/java/tomato/com/restaurant/model/RefreshToken.java
@@ -0,0 +1,35 @@
+package tomato.com.restaurant.model;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "refresh_token")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private Instant created;
+    private Instant expiry;
+    private boolean isRevoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
