6f55ba054c247ee511fcd6158bd042ce7595c911
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-29 00:59:07 +0530

    Add refreshtokenresponse and update jwt response accordingly

--- src/main/java/tomato/com/restaurant/response/RefreshTokenResponse.java
+++ src/main/java/tomato/com/restaurant/response/RefreshTokenResponse.java
@@ -1,5 +1,7 @@
 package tomato.com.restaurant.response;
 
+import java.time.Instant;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class JwtResponse {
-    private Long id;
-    private String jwt;
-    private RefreshTokenResponse refreshToken;
+public class RefreshTokenResponse {
+    private String token;
+    private Instant created;
+    private Instant expiry;
 }
