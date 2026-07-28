bdded034ce0754296fa256c7d57b011887fed45a
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-29 00:08:14 +0530

    Add findByName method in the interface

--- src/main/java/tomato/com/restaurant/repository/PermissionRepository.java
+++ src/main/java/tomato/com/restaurant/repository/PermissionRepository.java
@@ -1,9 +1,13 @@
 package tomato.com.restaurant.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tomato.com.restaurant.model.Permission;
 
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+
+    Optional<Permission> findByPermissionName(String permission);
 
 }
