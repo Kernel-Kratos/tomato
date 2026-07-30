3e2a95b8f9bc137ced5315a742e41d2cb6651cf8
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-07-30 23:57:53 +0530

    Update filterchain to add Ip Based rate limiter

--- src/main/java/tomato/com/restaurant/security/config/SecurityConfig.java
+++ src/main/java/tomato/com/restaurant/security/config/SecurityConfig.java
@@ -18,11 +18,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 import lombok.RequiredArgsConstructor;
-import tomato.com.restaurant.security.JWT.AuthTokenFilter;
 import tomato.com.restaurant.security.JWT.JwtAuthenticationEntryPoint;
-import tomato.com.restaurant.security.filter.RateLimitFilter;
+import tomato.com.restaurant.security.filter.AuthTokenFilter;
+import tomato.com.restaurant.security.filter.IpRateLimiterFilter;
+import tomato.com.restaurant.security.filter.JwtRateLimitFilter;
 import tomato.com.restaurant.security.user.CustomUserDetailsService;
 
 @RequiredArgsConstructor
@@ -62,8 +64,9 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth.requestMatchers(SECURED_URLS.toArray(String [] :: new)).authenticated()
             .anyRequest().permitAll());
             httpSecurity.authenticationProvider(daoAuthenticationProvider());
+            httpSecurity.addFilterBefore(new IpRateLimiterFilter(redisTemplate), LogoutFilter.class);
             httpSecurity.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-            httpSecurity.addFilterAfter(new RateLimitFilter(redisTemplate), AuthTokenFilter.class);
+            httpSecurity.addFilterAfter(new JwtRateLimitFilter(redisTemplate), AuthTokenFilter.class);
         return httpSecurity.build();
     } 
 } 
@@ -82,3 +85,11 @@ public class SecurityConfig {
 //AuthenticationManager is a manager and it delegates its task to many other methods. One of them is DaoAuthenticationProvider(DAP)
 //DAP gets customUserDetailsService so it knows to fetch the user from db
 // DAP also gets PasswordEncoder so it knows to compare the password given by user against stored hashPassword.  
+
+//Spring filter chain behaviour
+// Spring has 3 inbuilt filter chain: csrffilter, logoutfilter and usernamepasswordfilter and assigns them a strict priority.
+// so i tried to use my custom filter class(authtokenfilter()) for ipbasefilter but spring threw exception because it was not
+// defined or build yet so spring could not use it as anchor.
+// but it gets build at line 71 so spring can use it afterwards as it has assigned it order.  
+// my ipbased filter has to be built at very start i have to use Spring's filter and esp logout.
+// as priority (lowest-highest) : usernamepasswordfilter -> logoutfilter -> csrffilter 
