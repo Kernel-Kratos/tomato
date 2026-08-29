08205f44c72c530083d3d4448e1dd1f129c101a6
Kernel-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-08-29 22:30:43 +0530

    Add Email Dispacter util

---
+++ backend/src/main/java/tomato/com/tomato/utils/EmailDispacterUtil.java
@@ -0,0 +1,29 @@
+package tomato.com.tomato.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+public class EmailDispacterUtil {
+
+    @Value("${spring.mail.email}")
+    private static String mailSender;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void emailDispatch(String reciever, String subject, String body) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(mailSender);
+            mailMessage.setTo(reciever);
+            mailMessage.setSubject(subject);
+            mailMessage.setText(body);
+            javaMailSender.send(mailMessage);
+        } catch (Exception e) {
+           System.out.print("Error email was not send");
+        }
+    }
+}
